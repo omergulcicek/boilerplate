5b1f5c46fe3ab2263507792d7517f6ae306846d6
Ömer Gülçiçek <iletisim@omergulcicek.com>
2018-02-08 21:24:21 +0300

    browsersync
    canlı önizleme eklendi ve kod yapısında düzenlemeler gerçekleşti.

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,65 @@
 "use strict";
 
-var alphabetical = require("css-declaration-sorter");
-var autoprefixer = require("autoprefixer");
-var cleancss = require("gulp-clean-css");
-var concat = require("gulp-concat");
-var gulp = require("gulp");
-var postcss = require("gulp-postcss");
-var sass = require("gulp-sass");
-var uglify = require("gulp-uglify");
+var alphabetical    = require("css-declaration-sorter"),
+    autoprefixer    = require("autoprefixer"),
+    babel           = require("gulp-babel"),
+    browsersync     = require("browser-sync"),
+    cleancss        = require("gulp-clean-css"),
+    concat          = require("gulp-concat"),
+    gulp            = require("gulp"),
+    postcss         = require("gulp-postcss"),
+    sass            = require("gulp-sass"),
+    uglify          = require("gulp-uglify");
 
 /* --------------- */
 
-var cssFile = "assets/css/";
-var jsFile = "assets/js/";
+var source = {
+    sass: "public/scss/*.scss",
+    js: "public/js/*.js"
+}
 
-var cssSourceFiles = "public/scss/*.scss";
-var jsSourceFiles = "public/js/*.js";
-
-var cssFileName = "";
-var jsFileName = "";
+var output = {
+    css: "assets/css/",
+    cssName: "boilerplate.min.css",
+    js: "assets/js/",
+    jsName: "boilerplate.min.js"
+}
 
 /* --------------- */
+
+gulp.task("browsersync", function() {
+    browsersync({
+        server: {
+            baseDir: './'
+        },
+        open: false,
+        online: false,
+        notify: false
+    });
+});
 
 gulp.task("css", function() {
-    return gulp.src(cssSourceFiles)
+    return gulp.src(source.sass)
         .pipe(sass({outputStyle: "compressed"}))
         .pipe(postcss([autoprefixer()]))
-        .pipe(concat(cssFileName))
+        .pipe(concat(output.cssName))
         .pipe(postcss([alphabetical({order: "alphabetically"})]))
         .pipe(cleancss())
-        .pipe(gulp.dest(cssFile))
+        .pipe(gulp.dest(output.css))
+        .pipe(browsersync.reload({stream: true}))
 });
 
 gulp.task("javascript", function() {
-    return gulp.src(jsSourceFiles)
-        .pipe(concat(jsFileName))
+    return gulp.src(source.js)
+        .pipe(concat(output.jsName))
         .pipe(uglify())
-        .pipe(gulp.dest(jsFile))
+        .pipe(gulp.dest(output.js))
+        .pipe(browsersync.reload({stream: true}))
 });
 
 gulp.task("watch", function() {
-    gulp.watch(cssSourceFiles, ["css"]);
-    gulp.watch(jsSourceFiles, ["javascript"]);
+    gulp.watch(source.sass, ["css"]);
+    gulp.watch(source.js, ["javascript"]);
 });
 
-gulp.task("default", ["css", "javascript", "watch"]);
+gulp.task("default", ["css", "javascript", "browsersync", "watch"]);
